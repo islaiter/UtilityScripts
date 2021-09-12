1affb907913eddccdbc7a758a0c2fc9b1487e039
islaiter <45145344+islaiter@users.noreply.github.com>
2021-09-12 13:32:19 +0200

    Delete gascript.sh

--- gascript.sh
+++
@@ -1,8 +0,0 @@
-apt-get install build-essential gcc make perl dkms -y
-apt-get update
-apt-get upgrade -y
-mkdir /mnt/cdrom
-mount /dev/cdrom /mnt/cdrom
-cd /mnt/cdrom
-sh ./VBoxLinuxAdditions.run --nox11
-reboot
