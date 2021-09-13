0ec1f50044dbd0a3ce60e691188b6fb56639c82e
islaiter <45145344+islaiter@users.noreply.github.com>
2021-09-13 20:40:11 +0200

    Update gascript.sh
    Adding sudo to run commands and removing the forced reboot at the end

--- VBoxGuestAdditions/gascript.sh
+++ VBoxGuestAdditions/gascript.sh
@@ -1,8 +1,7 @@
-apt-get install build-essential gcc make perl dkms -y
-apt-get update
-apt-get upgrade -y
-mkdir /mnt/cdrom
-mount /dev/cdrom /mnt/cdrom
+sudo apt-get install build-essential gcc make perl dkms -y
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo mkdir /mnt/cdrom
+sudo mount /dev/cdrom /mnt/cdrom
 cd /mnt/cdrom
-sh ./VBoxLinuxAdditions.run --nox11
-reboot
+sudo sh ./VBoxLinuxAdditions.run --nox11
